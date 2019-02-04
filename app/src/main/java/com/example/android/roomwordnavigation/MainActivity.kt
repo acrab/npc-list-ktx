b7223c5a9f07424b43a6c52d87ef61ad5d16e001
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-02-04 16:31:14 +0000

    Cleanup - text fix - Suppress unused attribute warning - Update appCompat dependency - Remove unnecessary variables and imports - Fix database name

--- app/src/main/java/com/example/android/roomwordnavigation/MainActivity.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.android.roomwordnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,7 +11,6 @@ import com.example.android.roomwordnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -23,12 +19,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        drawerLayout = binding.drawerLayout
+        val drawerLayout = binding.drawerLayout
 
         navController = this.findNavController(R.id.mainNavHostFragment)
         appBarConfiguration = AppBarConfiguration(setOf(R.id.characterListFragment, R.id.organisationListFragment), drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/android/roomwordnavigation/data/AppDatabase.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/data/AppDatabase.kt
@@ -21,7 +21,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "Word_database"
+                    "app_database"
                 )
                     .build()
 
