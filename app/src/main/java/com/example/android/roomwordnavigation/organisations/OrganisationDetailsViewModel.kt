cc5d78df45284eb99aec0ce3e1bdce390b438198
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-02-06 09:35:13 +0000

    Remove duplicated android:label field Fix missing constructor parameter

--- app/src/main/java/com/example/android/roomwordnavigation/organisations/OrganisationDetailsViewModel.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/organisations/OrganisationDetailsViewModel.kt
@@ -1,20 +1,13 @@
 package com.example.android.roomwordnavigation.organisations
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import com.example.android.roomwordnavigation.ViewModelWithCoroutineScope
-import com.example.android.roomwordnavigation.data.Character
-import com.example.android.roomwordnavigation.data.MembershipRepository
-import com.example.android.roomwordnavigation.data.Organisation
-import com.example.android.roomwordnavigation.data.OrganisationMembership
-import kotlinx.coroutines.CoroutineScope
+import com.example.android.roomwordnavigation.data.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
-class OrganisationDetailsViewModel @Inject constructor(private val membershipRepository: MembershipRepository):ViewModelWithCoroutineScope()
+class OrganisationDetailsViewModel @Inject constructor(private val membershipRepository: MembershipRepository, private val organisationRepository: OrganisationRepository):ViewModelWithCoroutineScope()
 {
     private var organisationId = 0
 
