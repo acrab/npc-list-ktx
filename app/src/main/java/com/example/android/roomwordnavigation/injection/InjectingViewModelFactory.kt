1ef56b13530a9d40eb0c6fce995ccb37b88a6c07
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-02-08 16:34:42 +0000

    Add comments and spacing to make it a bit easier to read

--- app/src/main/java/com/example/android/roomwordnavigation/injection/InjectingViewModelFactory.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/injection/InjectingViewModelFactory.kt
@@ -7,20 +7,27 @@ import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
-class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>): ViewModelProvider.Factory {
+class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        //Try and find type by simple lookup
         var creator: Provider<out ViewModel>? = creators[modelClass]
+
         if (creator == null) {
+            //If not found, try and find a sub-class
             try {
                 creator = creators.entries.first { modelClass.isAssignableFrom(it.key) }.value
             } catch (e: NoSuchElementException) {
+                //Still not found: die
                 throw IllegalArgumentException("unknown model class $modelClass", e)
             }
         }
 
         try {
+            //Return it as the type requested
             return creator.get() as T
         } catch (e: Exception) {
+            //Or fail if somehow it's not castable.
             throw RuntimeException(e)
         }
     }
