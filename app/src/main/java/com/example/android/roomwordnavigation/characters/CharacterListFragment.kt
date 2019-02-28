fd7bec3af6f48cfbda699edff0ec61a65bd50050
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-02-28 10:26:43 +0000

    Tidy up: remove unused function - It wasn't creating a useful menu

--- app/src/main/java/com/example/android/roomwordnavigation/characters/CharacterListFragment.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/characters/CharacterListFragment.kt
@@ -1,16 +1,15 @@
 package com.example.android.roomwordnavigation.characters
 
 import android.os.Bundle
-import android.view.*
-import androidx.databinding.DataBindingUtil
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.roomwordnavigation.IWithViewModelFactory
-import com.example.android.roomwordnavigation.R
 import com.example.android.roomwordnavigation.databinding.FragmentCharacterListBinding
 import com.example.android.roomwordnavigation.ui.CharacterListAdapter
 import com.example.android.roomwordnavigation.ui.WithFAB
@@ -39,11 +38,6 @@ class CharacterListFragment : DaggerFragment(), IWithViewModelFactory, WithFAB {
         })
 
         return binding.root
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.overflow_menu, menu)
     }
 
     override fun onFABClicked(view: View) {
