68590d90b8f794dfa9306283f80c68b362918b0a
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-02-28 13:53:48 +0000

    Use AsyncListDiffer to update list with new items. - provides smoother visuals than just updating the list directly, and also

--- app/src/main/java/com/example/android/roomwordnavigation/ui/CharacterListAdapter.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/ui/CharacterListAdapter.kt
@@ -3,6 +3,8 @@ package com.example.android.roomwordnavigation.ui
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.roomwordnavigation.data.CharacterEntity
 import com.example.android.roomwordnavigation.databinding.CharacterListItemBinding
@@ -13,21 +15,33 @@ class CharacterListAdapter internal constructor(
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
 
-    private var characters = emptyList<CharacterEntity>()
+    private val listDiffer = AsyncListDiffer<CharacterEntity>(this, diffCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         return CharacterViewHolder(CharacterListItemBinding.inflate(inflater), onCharacterSelected)
     }
 
-    override fun getItemCount(): Int = characters.size
+    override fun getItemCount(): Int = listDiffer.currentList.size
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.setName(characters[position])
+        holder.setName(listDiffer.currentList[position])
     }
 
     override fun setData(data: List<CharacterEntity>) {
-        this.characters = data
-        notifyDataSetChanged()
+        listDiffer.submitList(data)
+    }
+
+    companion object {
+        val diffCallback = object : DiffUtil.ItemCallback<CharacterEntity>()
+        {
+            override fun areContentsTheSame(oldItem: CharacterEntity, newItem: CharacterEntity): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areItemsTheSame(oldItem: CharacterEntity, newItem: CharacterEntity): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
     }
 
     class CharacterViewHolder(
