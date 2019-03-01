9e1305ccf1b44c94f46711abf244299014e6d630
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-03-01 14:10:18 +0000

    Formatting

--- app/src/main/java/com/example/android/roomwordnavigation/data/MembershipDao.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/data/MembershipDao.kt
@@ -10,6 +10,9 @@ interface MembershipDao {
     @Insert
     fun createMembership(membership: OrganisationMembership)
 
-    @Query("SELECT character_table.* FROM character_table INNER JOIN characters_in_organisations_table ON character_table.id = characters_in_organisations_table.character WHERE characters_in_organisations_table.organisation = :organisationId")
+    @Query("""SELECT character_table.* FROM character_table
+        INNER JOIN characters_in_organisations_table ON character_table.id = characters_in_organisations_table.character
+        WHERE characters_in_organisations_table.organisation = :organisationId
+        ORDER BY character_table.name""")
     fun getMembers(organisationId: Int) : LiveData<List<CharacterEntity>>
 }
