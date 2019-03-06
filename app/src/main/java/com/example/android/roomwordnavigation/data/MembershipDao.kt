5192482ab2cbb066bfd47eccb0618b26b855de55
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-03-06 22:39:02 +0000

    WIP: created query and data object

--- app/src/main/java/com/example/android/roomwordnavigation/data/MembershipDao.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/data/MembershipDao.kt
@@ -10,6 +10,18 @@ interface MembershipDao {
     @Insert
     fun createMembership(membership: OrganisationMembership)
 
-    @Query("SELECT character_table.* FROM character_table INNER JOIN characters_in_organisations_table ON character_table.id = characters_in_organisations_table.character WHERE characters_in_organisations_table.organisation = :organisationId")
+    @Query("SELECT character_table.* FROM character_table" +
+            " INNER JOIN characters_in_organisations_table" +
+            " ON character_table.id = characters_in_organisations_table.character " +
+            "WHERE characters_in_organisations_table.organisation = :organisationId")
     fun getMembers(organisationId: Int) : LiveData<List<CharacterEntity>>
+
+    @Query("""
+        SELECT character_table.id characterId, character_table.name characterName, characters_in_organisations_table.organisation = :organisationId isMember from character_table
+        LEFT JOIN characters_in_organisations_table
+        ON character_table.id = characters_in_organisations_table.character
+    """)
+    fun getMembershipStatus(organisationId: Int) : LiveData<List<MembershipStatus>>
 }
+
+data class MembershipStatus(val characterId:Int, val characterName: String, val isMember:Boolean)
