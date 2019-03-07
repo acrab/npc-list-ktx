741159e75d00eff2a06227fe326fad9e9778d80a
Alex Crabbe <alex.crabbe@atombank.co.uk>
2019-03-07 17:03:16 +0000

    Tidy up: use built-in viewModelScope instead of creating new ones.

--- app/src/main/java/com/example/android/roomwordnavigation/ViewModelWithCoroutineScope.kt
+++ app/src/main/java/com/example/android/roomwordnavigation/ViewModelWithCoroutineScope.kt
@@ -1,17 +1,9 @@
 package com.example.android.roomwordnavigation
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlin.coroutines.CoroutineContext
+import androidx.lifecycle.viewModelScope
 
 abstract class ViewModelWithCoroutineScope : ViewModel()
 {
-    private var parentJob = Job()
-
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Main
-
-    protected val scope = CoroutineScope(coroutineContext)
+    protected val scope = viewModelScope
 }
